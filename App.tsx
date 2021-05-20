1fd03d29aa19a69ae28a9ebb7e9546236ebf94a0
Victor Queiroga <victorqueiroga.jp@gmail.com>
2021-05-20 18:27:56 -0300

    testes ok

--- App.tsx
+++ App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+// import FlashMessage from 'react-native-flash-message';
 import { Home } from './src/pages/Home';
 
 export default function App() {
-  return (
-    <>
-      <StatusBar 
-        backgroundColor="transparent" 
-        translucent 
-        barStyle="light-content" 
-      />
-      <Home />
-    </>
-  );
+	return (
+		<>
+			<StatusBar backgroundColor="transparent" translucent barStyle="light-content" />
+
+			<Home />
+			{/* <FlashMessage position="bottom" /> */}
+		</>
+	);
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,39 +3,69 @@ import React, { useState } from 'react';
 import { Header } from '../components/Header';
 import { MyTasksList } from '../components/MyTasksList';
 import { TodoInput } from '../components/TodoInput';
+import nextId, { setPrefix } from 'react-id-generator';
+import { Alert } from 'react-native';
+// import { showMessage, hideMessage } from 'react-native-flash-message';
 
 interface Task {
-  id: number;
-  title: string;
-  done: boolean;
+	id: number;
+	title: string;
+	done: boolean;
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+	const [tasks, setTasks] = useState<Task[]>([]);
 
-  function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
-  }
+	setPrefix('');
 
-  function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
-  }
+	async function handleAddTask(newTaskTitle: string) {
+		//TODO - add new task if it's not empty
+		if (newTaskTitle.trim() === '') {
+			return;
+		}
+		if (checkDuplicated(newTaskTitle)) {
+			/* await showMessage({
+				message: 'Task já existe',
+				type: 'danger',
+				icon: 'danger',
+			}); */
+			return;
+		}
 
-  function handleRemoveTask(id: number) {
-    //TODO - remove task from state
-  }
+		const newTask = { id: Number(nextId()), title: newTaskTitle, done: false } as Task;
 
-  return (
-    <>
-      <Header />
+		setTasks((oldTasks) => [...oldTasks, newTask]);
+	}
 
-      <TodoInput addTask={handleAddTask} />
+	function handleMarkTaskAsDone(id: number) {
+		//TODO - mark task as done if exists
+		const updatedTasks = [...tasks];
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
-      />
-    </>
-  )
+		let doneTask = updatedTasks.find((task) => task.id === id);
+		if (doneTask) {
+			doneTask.done = !doneTask.done;
+			setTasks(updatedTasks);
+		}
+	}
+
+	function handleRemoveTask(id: number) {
+		//TODO - remove task from state
+		const updatedTasks = tasks.filter((task) => task.id !== id);
+
+		setTasks(updatedTasks);
+	}
+
+	function checkDuplicated(newTask: string): boolean {
+		return tasks.findIndex((task) => task.title.trim() === newTask.trim()) > -1;
+	}
+
+	return (
+		<>
+			<Header />
+
+			<TodoInput addTask={handleAddTask} />
+
+			<MyTasksList tasks={tasks} onPress={handleMarkTaskAsDone} onLongPress={handleRemoveTask} />
+		</>
+	);
 }
